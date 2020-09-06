98e96f3f9b36849b96ac7eb06818e0e9f2ff42b9
Nhựt <nhutcaucui@gmail.com>
2020-09-06 16:32:59 +0700

    zoom with rotation for top down, pan camera for mobile top down

--- Assets/Scripts/TopDownCameraControl.cs
+++ Assets/Scripts/TopDownCameraControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TopDownCamera : MonoBehaviour
+public class TopDownCameraControl : MonoBehaviour
 {
     public float initX = 0f;
     public float initZ = 0f;
@@ -18,10 +18,15 @@ public class TopDownCamera : MonoBehaviour
     public float bottomBound = -5f;
     public float rightBound = 5f;
     public float leftBound = -5f;
+    public GameObject target;
     float currentHeight;
+    float currentAngle;
+    float angleStep;
     // Start is called before the first frame update
+    
     void Start()
     {
+        currentAngle = initAngle;
         if(maxHeight< minHeight){
             maxHeight = minHeight;
         }
@@ -32,19 +37,47 @@ public class TopDownCamera : MonoBehaviour
             initHeight = maxHeight;
         }
         currentHeight = initHeight;
+        if(maxHeight != minHeight){
+            angleStep = initAngle/((maxHeight - minHeight) / zoomStep);
+            
+        }else{
+            angleStep = initAngle;
+        }
         transform.position = new Vector3(initX,currentHeight,initZ);
         Quaternion rotation = Quaternion.AngleAxis(initAngle,Vector3.right);
         transform.rotation = rotation;
         Cursor.lockState = CursorLockMode.Confined;
     }
 
+     private void Update() {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f && currentHeight < maxHeight)
+        {
+            currentHeight = Mathf.Clamp(currentHeight + zoomStep, minHeight, maxHeight);
+            currentAngle = Mathf.Clamp(currentAngle+angleStep, 0f, initAngle);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f && currentHeight > minHeight)
+        {
+            currentHeight = Mathf.Clamp(currentHeight - zoomStep, minHeight, maxHeight);
+            currentAngle = Mathf.Clamp(currentAngle - angleStep, 0f, initAngle);
+        }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Vector3 jump = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - (currentHeight * Mathf.Tan(initAngle)));
+            transform.position = jump;
+        }
+    }
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if(transform.position.y > currentHeight || transform.position.y < currentHeight){
             float height = Mathf.MoveTowards(transform.position.y, currentHeight, Time.deltaTime/zoomSpeed);
             Vector3 pos = new Vector3(transform.position.x, height, transform.position.z);
             transform.position = pos;
+        }
+        if(transform.eulerAngles.x > currentAngle || transform.eulerAngles.x < currentAngle){
+            Debug.Log(transform.eulerAngles.x);
+            float rot = Mathf.MoveTowards(transform.eulerAngles.x, currentAngle, 10f * Time.deltaTime / zoomSpeed);
+            Quaternion q = Quaternion.Euler(rot, transform.eulerAngles.y, transform.eulerAngles.z);
+            transform.rotation = q;
         }
         if(transform.position.z > topBound){
             Vector3 pos = new Vector3(transform.position.x,transform.position.y, topBound);
@@ -78,13 +111,6 @@ public class TopDownCamera : MonoBehaviour
         {
             transform.Translate(-(Vector3.right * Time.deltaTime * speed), Space.World);
         }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f && currentHeight < maxHeight)
-        {
-            currentHeight = Mathf.Clamp(currentHeight + zoomStep, minHeight, maxHeight);
-        }
-        else if (Input.GetAxis("Mouse ScrollWheel") > 0f && currentHeight > minHeight)
-        {
-            currentHeight = Mathf.Clamp(currentHeight - zoomStep, minHeight, maxHeight);
-        }
+        
     }
 }
